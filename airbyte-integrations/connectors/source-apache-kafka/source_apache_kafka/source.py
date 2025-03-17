6a77bce995f89a5f1cc330718a5d80889f506a13
Brandon Schow <bschow@adobe.com>
2025-03-17 16:49:26 -0700

    working intial tests

--- airbyte-integrations/connectors/source-apache-kafka/source_apache_kafka/source.py
+++ airbyte-integrations/connectors/source-apache-kafka/source_apache_kafka/source.py
@@ -1,14 +1,16 @@
-from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple, Union
+from typing import Any, Iterable, List, Mapping, Optional, Tuple, Union
 
-import requests
 import logging
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.models import SyncMode, AirbyteMessage
+from airbyte_cdk.sources.streams.core import package_name_from_class
+from airbyte_cdk.models import SyncMode, AirbyteMessage, AirbyteCatalog
+from airbyte_cdk.sources.utils.schema_helpers import ResourceSchemaLoader
+
+from time import sleep
+
 import confluent_kafka
-from confluent_kafka.serialization import SerializationContext, MessageField
-from confluent_kafka.schema_registry.json_schema import JSONDeserializer
-import json
+from mockafka import FakeConsumer
 
 StreamData = Union[Mapping[str, Any], AirbyteMessage]
 
@@ -34,8 +36,15 @@ class SourceApacheKafka(AbstractSource):
         logger.info(f"Kafka connection success")
         return True, None
 
+    def discover(self, logger: logging.Logger, config: Mapping[str, Any]) -> AirbyteCatalog:
+        """Implements the Discover operation from the Airbyte Specification.
+        See https://docs.airbyte.com/understanding-airbyte/airbyte-protocol/#discover.
+        """
+        streams = [stream.as_airbyte_stream() for stream in self.streams(config=config)]
+        return AirbyteCatalog(streams=streams)
+
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
-        return [Topic('test.topic', config)]
+        return [Topic(topic_name, config) for topic_name in config['topics'].split(',')]
 
 
 class Topic(Stream):
@@ -44,8 +53,27 @@ class Topic(Stream):
 
     def __init__(self, topic_name: str, config: dict, **kwargs):
         super().__init__(**kwargs)
-        self.topic_name = topic_name
+        self.name = topic_name
         self.config = config
+
+    def get_json_schema(self) -> Mapping[str, Any]:
+        # overrides super function to just get `topic.json` since all topics will have the same schema
+        return ResourceSchemaLoader(package_name_from_class(self.__class__)).get_schema('topic')
+
+    def get_consumer(self, sync_mode):
+        if self.config.get('is_test'):
+            logger.warning("`is_test` is true, using FakeConsumer for testing")
+            return FakeConsumer()
+        kafka_consumer_config = {
+            "auto.offset.reset": 'latest',
+            "bootstrap.servers": self.config['bootstrap_servers'],
+            "group.id": 'test_group',
+            "enable.auto.commit": False,
+            "logger": logger
+        }
+        logger.info("CONSUMER CONFIG")
+        logger.info(kafka_consumer_config)
+        return confluent_kafka.Consumer(kafka_consumer_config)
 
     def read_records(
             self,
@@ -54,30 +82,38 @@ class Topic(Stream):
             stream_slice: Optional[Mapping[str, Any]] = None,
             stream_state: Optional[Mapping[str, Any]] = None,
     ) -> Iterable[StreamData]:
+        logger.info("Getting consumer")
         consumer = self.get_consumer(sync_mode)
-        consumer.subscribe([self.topic_name])
+        offset = confluent_kafka.OFFSET_BEGINNING
+        consumer.subscribe([self.name], on_assign=_seek_to_offset_callback(offset))
+        logger.info("Sleeping while subscription process")
+        sleep(1)
         try:
-            messages = consumer.consume()
-            for message in messages:
-                print('Message:')
-                print(message.value())
+            message = consumer.poll(timeout=3)
+            while message:
                 value =message.value().decode("utf-8")
                 key = message.key().decode("utf-8")
                 yield {'key': key, 'value': value}
+                message = consumer.poll(timeout=3)
         finally:
             consumer.close()
 
-    def get_consumer(self, sync_mode):
-        if sync_mode == SyncMode.full_refresh:
-            offset = 'earliest'
-        else:
-            offset = 'latest'
-        kafka_consumer_config = {
-            "auto.offset.reset": offset,
-            "bootstrap.servers": self.config['bootstrap_servers'],
-            "group.id": 'test_group',
-            "enable.auto.commit": False,
-            "logger": logger
-        }
-        return confluent_kafka.Consumer(kafka_consumer_config)
+def get_offset(sync_mode: SyncMode, stream_state):
+    if sync_mode == SyncMode.incremental:
+        raise NotImplemented("Need to implement")
+    if sync_mode == SyncMode.full_refresh:
+        return confluent_kafka.OFFSET_BEGINNING
+    raise AssertionError("Configuration not supported for offset seeking")
+
+
+def _seek_to_offset_callback(offset):
+    """ Used by the consumer to seek to a specific offset in the topic """
+
+    def on_assign(consumer, partitions):
+        for p in partitions:
+            logger.info(f"Setting partition {p} to offset {offset}")
+            p.offset = offset
+        consumer.assign(partitions)
+
+    return on_assign
 
